2bbf7bcca08fad34dc9773743b8206887dc93831
nainaistar <nainaistar@gmail.com>
2019-10-19 22:42:44 +0900

    [Add] repositoryのテスト

---
+++ library-backend-service/src/test/java/com/example/library/infra/LendingRecordRepositoryImplTest.java
@@ -0,0 +1,47 @@
+package com.example.library.infra;
+
+import com.example.library.biz.domain.Isbn;
+import com.example.library.biz.domain.LendingRecord;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class LendingRecordRepositoryImplTest {
+    LendingRecordRepository target;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    LendingRecord lendingRecord;
+
+    @BeforeEach
+    void setUp() {
+        target = new LendingRecordRepositoryImpl(jdbcTemplate);
+    }
+
+    @Test
+    void insert() {
+        LendingRecord entity = this.lendingRecord.create(new Isbn("1"), "2");
+
+        target.insert(entity);
+
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("SELECT * FROM LENDING_RECORD");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(maps).hasSize(1);
+        softly.assertThat(maps.get(0).get("ISBN")).isEqualTo("1");
+        softly.assertThat(maps.get(0).get("USER_ID")).isEqualTo("2");
+        softly.assertAll();
+    }
+}
