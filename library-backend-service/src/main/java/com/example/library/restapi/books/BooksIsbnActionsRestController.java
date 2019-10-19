d5043fc82db2c27ab187e712ee603a7587201a03
nainaistar <nainaistar@gmail.com>
2019-10-19 15:43:57 +0900

    [Add] requestBodyと簡単なvalidatorを用意

--- library-backend-service/src/main/java/com/example/library/restapi/books/BooksIsbnActionsRestController.java
+++ library-backend-service/src/main/java/com/example/library/restapi/books/BooksIsbnActionsRestController.java
@@ -1,27 +1,30 @@
 package com.example.library.restapi.books;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@Validated
 public class BooksIsbnActionsRestController {
 
     @PostMapping("books/{isbn}/actions")
-    public ResponseEntity<Void> actions(@PathVariable("isbn") String isbn
-//            , @RequestBody RequestParam body
-    ) {
+    public ResponseEntity<Void> actions(@PathVariable("isbn") String isbn, @RequestBody @Valid RequestParam body) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
+    // FIXME: setterはいらないけど、valueだときかなかった。
+    @Data
     private static class RequestParam {
+        @NotNull
         private String type;
 
     }
--- library-backend-service/src/test/java/com/example/library/functionalTest/BooksIsbnActionsRestControllerTest.java
+++ library-backend-service/src/test/java/com/example/library/functionalTest/BooksIsbnActionsRestControllerTest.java
@@ -2,12 +2,17 @@ package com.example.library.functionalTest;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BooksIsbnActionsRestControllerTest {
@@ -15,18 +20,54 @@ class BooksIsbnActionsRestControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @DisplayName("１冊の本を借りる")
-    @Test
-    void test01() {
+    @Nested
+    class 貸りる {
+        @DisplayName("１冊の本を借りる")
+        @Test
+        void test01() throws URISyntaxException {
 
-        final String isbn = "1111111111";
+            final String isbn = "1111111111";
 
-        String url = "/v1/books/" + isbn + "/actions";
-        ResponseEntity<Object> response = restTemplate.postForEntity(url, null, null);
+            //language=JSON
+            String request = "{" +
+                    "\"type\":\"a\"" +
+                    "}";
 
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        softly.assertAll();
+            URI url = URI.create("/v1/books/" + isbn + "/actions");
 
+            RequestEntity requestEntity =
+                    RequestEntity.post(url).contentType(MediaType.APPLICATION_JSON_UTF8).body(request);
+
+            ResponseEntity<Object> response =
+                    restTemplate.exchange(requestEntity, Object.class);
+
+            SoftAssertions softly = new SoftAssertions();
+            softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            softly.assertAll();
+        }
+    }
+
+
+    @Nested
+    class Validator {
+        @DisplayName("パラメータのtypeがNotNull制約に引っかかる")
+        @Test
+        void test01() throws URISyntaxException {
+
+            final String isbn = "1111111111";
+
+            //language=JSON
+            String request = "{}";
+
+            URI url = URI.create("/v1/books/" + isbn + "/actions");
+
+            RequestEntity requestEntity =
+                    RequestEntity.post(url).contentType(MediaType.APPLICATION_JSON_UTF8).body(request);
+
+            ResponseEntity<Object> response =
+                    restTemplate.exchange(requestEntity, Object.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        }
     }
 }
