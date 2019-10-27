433a3011479a9c0151cea2321fb60adb6a64c128
nainaistar <nainaistar@gmail.com>
2019-10-28 00:55:17 +0900

    [Add] WIP:画面の修正

--- library-frontend-service/src/App.vue
+++ library-frontend-service/src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
       >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Search</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>貸出一覧</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline">
+        <span>ADC</span>
+        <span class="font-weight-light">Library</span>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-content>
@@ -31,7 +50,14 @@ export default Vue.extend({
     HelloWorld,
   },
   data: () => ({
+    drawer: false,
     //
   }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 });
 </script>
