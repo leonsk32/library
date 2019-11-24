d3007cec3f5dac1d721098600063bcd50b2666cd
nainaistar <nainaistar@gmail.com>
2019-11-24 23:02:29 +0900

    [Add] add icon

--- library-frontend-service/src/class/LendingRecord.ts
+++ library-frontend-service/src/class/LendingRecord.ts
@@ -12,6 +12,10 @@ export default class LendingRecord {
     this.title = '';
   }
 
+  /**
+   * openBd用のisbn整形ロジック。
+   * 本来であれば、腐敗防止層の持ち物だが、他のAPIでも使用する可能性を考慮してこちらのドメインに持たせる。
+   */
   getFormattedIsbn() : string {
     return `${this.isbn.substr(0, 3)}-${this.isbn.substr(3, 1)}-${this.isbn.substr(4, 4)}-${this.isbn.substr(8, 4)}-${this.isbn.substr(12, 1)}`;
   }
---
+++ library-frontend-service/tests/unit/lendingRecord.spec.ts
@@ -0,0 +1,16 @@
+import LendingRecord from "@/class/LendingRecord";
+
+describe('openBd用のisbn整形ロジック', () => {
+  it('エリックエバンスのDDD本を目的通りに成形する', () => {
+    const expected: string = '978-4-7981-2196-3';
+    const lendingRecord = new LendingRecord('9784798121963','');
+    expect(lendingRecord.getFormattedIsbn()).toMatch(expected);
+  });
+
+  it('実践DDDの本を目的通りに成形する', () => {
+    const expected: string = '978-4-7981-3161-0';
+    const lendingRecord = new LendingRecord('9784798131610','');
+    expect(lendingRecord.getFormattedIsbn()).toMatch(expected);
+  });
+});
+
