d409644be81de3aea41d947f0363cb83fd47e708
ryo <sigaryoutakaki@gmail.com>
2019-11-24 14:26:49 +0900

    BooksTestをリファクタ

--- library-backend-service/src/test/java/com/example/library/restapi/BooksTest.java
+++ library-backend-service/src/test/java/com/example/library/restapi/BooksTest.java
@@ -2,6 +2,7 @@ package com.example.library.restapi;
 
 import com.example.library.app_service.BookService;
 import com.example.library.domain.book.Book;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -25,12 +26,15 @@ class BooksTest {
     @MockBean
     private BookService service;
 
+    @DisplayName("本の全件検索")
     @Test
     void test01() throws Exception {
+        // arrange
         List<Book> books = asList(
                 new Book("9784567890123"),
                 new Book("9784567890124"));
 
+        // act and assert
         when(service.searchAll()).thenReturn(books);
 
         mockMvc.perform(get("/v1/books"))
@@ -44,22 +48,27 @@ class BooksTest {
                                 "}", true));
     }
 
+    @DisplayName("本の登録")
     @Test
     void test02() throws Exception {
+        // arrange
         final String isbn = "9784567890123";
         doNothing().when(service).register(isbn);
 
+        // act and assert
         mockMvc.perform(put("/v1/books/" + isbn)
         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
         verify(service).register(isbn);
     }
 
+    @DisplayName("本の排棄")
     @Test
     void test03() throws Exception {
+        // arrange
         final String isbn = "9784567890123";
+        // act and assert
         doNothing().when(service).waste(isbn);
-
         mockMvc.perform(delete("/v1/books/" + isbn)
         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
