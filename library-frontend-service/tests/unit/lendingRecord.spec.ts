27e1df6ab8ca0896580b76edea63bd69316b54ce
nainaistar <nainaistar@gmail.com>
2019-11-28 18:32:54 +0900

    [Refactor] テストコードにlint

--- library-frontend-service/tests/unit/lendingRecord.spec.ts
+++ library-frontend-service/tests/unit/lendingRecord.spec.ts
@@ -1,16 +1,15 @@
-import LendingRecord from "@/class/LendingRecord";
+import LendingRecord from '@/class/LendingRecord';
 
 describe('openBd用のisbn整形ロジック', () => {
   it('エリックエバンスのDDD本を目的通りに成形する', () => {
     const expected: string = '978-4-7981-2196-3';
-    const lendingRecord = new LendingRecord('9784798121963','');
+    const lendingRecord = new LendingRecord('9784798121963', '');
     expect(lendingRecord.getFormattedIsbn()).toMatch(expected);
   });
 
   it('実践DDDの本を目的通りに成形する', () => {
     const expected: string = '978-4-7981-3161-0';
-    const lendingRecord = new LendingRecord('9784798131610','');
+    const lendingRecord = new LendingRecord('9784798131610', '');
     expect(lendingRecord.getFormattedIsbn()).toMatch(expected);
   });
 });
-
