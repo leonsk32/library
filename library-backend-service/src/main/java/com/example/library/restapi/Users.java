074275b39e3b07bb665568e6628a31a01530eef1
nainaistar <nainaistar@gmail.com>
2019-12-01 21:47:10 +0900

    [Refactor] unused

--- library-backend-service/src/main/java/com/example/library/restapi/Users.java
+++ library-backend-service/src/main/java/com/example/library/restapi/Users.java
@@ -14,7 +14,6 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
