0064d31513befae93f222c1018979adde18137dd
ryo <sigaryoutakaki@gmail.com>
2019-12-08 14:05:59 +0900

    ロがーを仕込んだ

--- library-backend-service/src/main/java/com/example/library/restapi/Books.java
+++ library-backend-service/src/main/java/com/example/library/restapi/Books.java
@@ -5,6 +5,7 @@ import com.example.library.app_service.BookService;
 import com.example.library.domain.book.Book;
 import com.example.library.restapi.dto.BooksDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class Books implements BooksApi {
     private final BookService service;
 
@@ -23,6 +25,7 @@ public class Books implements BooksApi {
     @Override
     public ResponseEntity<BooksDto> booksGet() {
         List<Book> books = service.searchAll();
+        log.info("enter booksGet");
         return new ResponseEntity<>(convert(books), OK);
     }
 
