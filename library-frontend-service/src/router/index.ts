e0a40cd62a91982b87e6c97f8a290510f6aa5c9a
nainaistar <nainaistar@gmail.com>
2019-12-09 01:44:10 +0900

    [Add] 棚卸用views追加

--- library-frontend-service/src/router/index.ts
+++ library-frontend-service/src/router/index.ts
@@ -16,6 +16,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Books.vue'),
   },
   {
+    path: '/books/inventory',
+    name: 'inventory',
+    component: () => import(/* webpackChunkName: "inventory" */ '../views/BooksInventory.vue'),
+  },
+  {
     path: '/search',
     name: 'search',
     component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue'),
