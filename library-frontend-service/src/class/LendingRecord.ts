b8fe5b16965f7dca5cd984e46111f13eeb6e3f0a
nainaistar <nainaistar@gmail.com>
2019-12-13 16:43:52 +0900

    [Add] 貸出者の名前を出すようにした。

--- library-frontend-service/src/class/LendingRecord.ts
+++ library-frontend-service/src/class/LendingRecord.ts
@@ -7,9 +7,22 @@ export default class LendingRecord {
 
   userId: string;
 
-  constructor(isbn: Isbn, userId: string, title: string) {
+  familyName: string;
+
+  givenName: string;
+
+  fullName: string;
+
+  constructor(isbn: Isbn, userId: string, title: string, familyName: string, givenName: string) {
     this.isbn = isbn;
     this.userId = userId;
     this.title = title;
+    this.familyName = familyName;
+    this.givenName = givenName;
+    this.fullName = this.getFullName();
+  }
+
+  private getFullName() : string {
+    return this.familyName + this.givenName;
   }
 }
--- library-frontend-service/src/components/BookList.vue
+++ library-frontend-service/src/components/BookList.vue
@@ -30,7 +30,8 @@ export default class BookList extends Vue {
 
   headers: any = [
     { text: 'タイトル', value: 'title' },
-    { text: '貸出者', value: 'userId' },
+    { text: 'userId', value: 'userId' },
+    { text: 'user', value: 'fullName' },
   ];
 
     bookApi: LendingRecordsApi = new LendingRecordsApi(customConfiguration);
@@ -69,7 +70,11 @@ export default class BookList extends Vue {
     */
     async createLendingRecord(lendingRecordDto : any): Promise<void> {
       const isbn = new Isbn(lendingRecordDto.isbn);
-      const lendingRecord = new LendingRecord(isbn, lendingRecordDto.userId, await isbn.getTitle());
+      const lendingRecord = new LendingRecord(isbn,
+        lendingRecordDto.userId,
+        await isbn.getTitle(),
+        lendingRecordDto.simei,
+        lendingRecordDto.namae);
       this.lendingRecords.push(lendingRecord);
     }
 }
